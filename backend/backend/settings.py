6f4e05d7dbdb40d692021ae17eed26ac5682eb72
Jeonghye Choi <ppllhm.0@gmail.com>
2021-06-24 04:13:58 +0900

    feat: allow all IP to access to Django webServer

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-ruhv$w-9j#vk5*yu@v*!@12y)(hb*a^s+or1nb!xw56=utqe!8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
