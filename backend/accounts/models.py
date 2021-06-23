60903d66e4949e3ffdf5c4a20f60bf75963999c5
Dong-Young Kim <31337.persona@gmail.com>
2021-06-23 20:52:28 +0900

    refactor(accounts): change field names

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,15 +3,18 @@ from django.db import models
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, user_id, user_name, user_stuid, user_pw):
-        user = self.model(user_id=user_id, user_name=user_name, user_stuid=user_stuid)
-        user.set_password(user_pw)
+    def create_user(self, username, user_name, user_stuid, password):
+        user = self.model(username=username, user_name=user_name, user_stuid=user_stuid)
+        user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, user_id, user_pw, user_name, user_stuid):
+    def create_superuser(self, username, password, user_name, user_stuid):
         user = self.create_user(
-            user_id=user_id, user_pw=user_pw, user_name=user_name, user_stuid=user_stuid
+            username=username,
+            password=password,
+            user_name=user_name,
+            user_stuid=user_stuid,
         )
         user.is_superuser = True
         user.save(using=self._db)
@@ -19,18 +22,18 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser):
-    user_id = models.CharField(unique=True, max_length=16)
-    user_pw = models.CharField(max_length=32)
+    username = models.CharField(unique=True, max_length=16)
+    password = models.CharField(max_length=32)
     user_name = models.CharField(max_length=16)
     user_stuid = models.CharField(max_length=9)
 
     objects = UserManager()
 
-    USERNAME_FIELD = "user_id"
-    REQUIRED_FIELDS = ["user_pw", "user_name", "user_stuid"]
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["password", "user_name", "user_stuid"]
 
     def __str__(self) -> str:
-        return self.user_id
+        return self.username
 
     @property
     def is_staff(self):
