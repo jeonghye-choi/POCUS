13bc36e8670faf9758ba77415e66df22ee4bacdd
Dong-Young Kim <31337.persona@gmail.com>
2021-06-24 13:53:59 +0900

    fix(accounts): fix user model for superuser

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,31 +1,61 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, user_name, user_stuid, password):
-        user = self.model(username=username, user_name=user_name, user_stuid=user_stuid)
-        user.set_password(password)
+    use_in_migrations = True
+
+    def _create_user(self, username, password, **extra_fields):
+        if not username:
+            raise ValueError("The given username must be set")
+        user = self.model(username=username, **extra_fields)
+        user.password = make_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, password, user_name, user_stuid):
-        user = self.create_user(
-            username=username,
-            password=password,
-            user_name=user_name,
-            user_stuid=user_stuid,
-        )
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
+    def create_user(self, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(username, password, **extra_fields)
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(username, password, **extra_fields)
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(unique=True, max_length=16)
     password = models.CharField(max_length=32)
     user_name = models.CharField(max_length=16)
     user_stuid = models.CharField(max_length=9)
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
 
     objects = UserManager()
 
@@ -34,7 +64,3 @@ class User(AbstractBaseUser):
 
     def __str__(self) -> str:
         return self.username
-
-    @property
-    def is_staff(self):
-        return self.is_staff
