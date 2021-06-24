0a8c5f760c9ce3dcc97aa33ca32ffa23cbc0bc80
Jeonghye Choi <ppllhm.0@gmail.com>
2021-06-25 04:25:01 +0900

    feat: add 'Keyword' model and migrate

--- backend/keywords/models.py
+++ backend/keywords/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Keyword(models.Model):
+    user_id = models.IntegerField(unique=True)
+    keyword = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+
+    REQUIRED_FIELDS = ["user_id", "keyword", "category"]
+
+    def __str__(self) -> str:
+        return self.keyword
