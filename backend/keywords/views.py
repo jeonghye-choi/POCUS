cff11fe36c9403d8c5bf1d7bb45effdd978c133e
Jeonghye Choi <ppllhm.0@gmail.com>
2021-06-25 10:52:01 +0900

    feat: add keyword create view function

--- backend/keywords/views.py
+++ backend/keywords/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.serializers import SerializerMetaclass
 
-# Create your views here.
+from .models import Keyword
+from .serializers import KeywordSerializer
+
+@api_view(['GET'])
+def keyword_create(request):
+    '''
+    GET /api/v1/keywords
+    '''
+    if request.method == 'GET':
+        # 1. 디비에 있는 모든 keywords를 가져온다.
+        # 2. 전송가능한 형태(JSON)으로 바꿔준다.
+        # 3. 마지막으로 Response 함수를 이용해 반환
+        keywords = Keyword.objects.all()
+        serializer = KeywordSerializer(keywords, many=True)
+        
+        return Response(data=serializer.data)
