d44f3acb160a5ca4d97360e26691732400e95958
Jeonghye Choi <ppllhm.0@gmail.com>
2021-07-03 00:51:35 +0900

    feat: join keyword model with user model

--- backend/keywords/models.py
+++ backend/keywords/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from accounts.models import User
 
 class Keyword(models.Model):
-    user_id = models.IntegerField(unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     keyword = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
 
