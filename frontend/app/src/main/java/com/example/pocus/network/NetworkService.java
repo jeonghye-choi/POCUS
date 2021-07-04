799314bc8a03f68dd7830533178f9b37912f90ea
Jeonghye Choi <ppllhm.0@gmail.com>
2021-07-04 16:04:53 +0900

    fix(networkservice): fix request api url

--- frontend/app/src/main/java/com/example/pocus/network/NetworkService.java
+++ frontend/app/src/main/java/com/example/pocus/network/NetworkService.java
@@ -1,19 +1,17 @@
 package com.example.pocus.network;
-
-import android.os.Build;
-
 import java.util.List;
 
-import retrofit2.Call;
+//import okhttp3.RequestBody;
+//import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
+//import retrofit2.http.Field;
+//import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.PATCH;
 import retrofit2.http.POST;
-import retrofit2.http.PUT;
-import retrofit2.http.Part;
+//import retrofit2.http.PUT;
+//import retrofit2.http.Part;
 import retrofit2.http.Path;
 
 public interface NetworkService {
@@ -21,31 +19,31 @@ public interface NetworkService {
     @POST("/keywords/")
     Call<Keyword> post_keyword(@Body Keyword keyword);
 
-    @PATCH("/keywords/{pk}")
+    @PATCH("/keywords/{pk}/")
     Call<Keyword> patch_keyword(@Path("") int pk, @Body Keyword keyword);
 
-    @DELETE("/keywords/{pk}")
+    @DELETE("/keywords/{pk}/")
     Call<Keyword> delete_keyword(@Path("") int pk);
 
     @GET("/keywords/")
     Call<List<Keyword>> get_keyword();
 
-    @GET("/keywords/{pk}")
+    @GET("/keywords/{pk}/")
     Call<Keyword> get_pk_keyword(@Path("") int pk);
 
     // messages api interface
     @POST("/messages/")
     Call<Message> post_message(@Body Message message);
 
-    @PATCH("/messages/{pk}")
+    @PATCH("/messages/{pk}/")
     Call<Message> patch_message(@Path("") int pk, @Body Message message);
 
-    @DELETE("/messages/{pk}")
+    @DELETE("/messages/{pk}/")
     Call<Message> delete_message(@Path("") int pk);
 
     @GET("/messages/")
     Call<List<Message>> get_message();
 
-    @GET("/messages/{pk}")
+    @GET("/messages/{pk}/")
     Call<Message> get_pk_message(@Path("") int pk);
 }
